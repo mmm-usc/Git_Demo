08dd6552a5d859acf143b0f69d83a989d3cf4c1a
Yichizhang2024 <yzhang97@usc.edu>
2022-09-06 17:18:31 -0700

    add and.R file

---
+++ and.R
@@ -0,0 +1,13 @@
+# Create an R file “and.R” which has one function “checkifand” which takes in one 
+# argument. Returns TRUE if the argument provided is “and” and FALSE if there is no match.
+
+checkifand <- function(word){
+  if(word == "and"){
+    return("TRUE")
+  }else{
+    return("FALSE")
+  }
+}
+# test 
+checkifand("no")
+checkifand("and")
